edb467d93548cc77730a857e4789957dfcff0112
Stefan Boicu <stefanboicu1@gmail.com>
2021-10-13 15:44:23 +0300

    Initialized empty spring project with some dependencies

---
+++ src/main/java/com/utm/mementomori/MementomoriApplication.java
@@ -0,0 +1,13 @@
+package com.utm.mementomori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MementomoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MementomoriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utm/mementomori/MementomoriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utm.mementomori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MementomoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
