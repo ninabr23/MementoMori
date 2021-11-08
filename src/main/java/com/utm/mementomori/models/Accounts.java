dad11471b9986aefb5ef28808a8441adf679afd4
Liviu <liviumocanu20009@gmail.com>
2021-11-08 19:33:51 +0200

    Temporary changes to index

--- src/main/java/com/utm/mementomori/models/Accounts.java
+++ src/main/java/com/utm/mementomori/models/Accounts.java
@@ -1,6 +1,7 @@
 package com.utm.mementomori.models;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "accounts")
@@ -21,6 +22,38 @@ public class Accounts {
     @Column(name = "password")
     private String password;
 
+    public Accounts() {
+    }
+
+    public Accounts(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, password);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Accounts account = (Accounts) o;
+        return Objects.equals(id, account.id) &&
+                Objects.equals(email, account.email) &&
+                Objects.equals(password, account.password);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
     public void setId(Integer id) {
         this.id = id;
     }
@@ -31,6 +64,10 @@ public class Accounts {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 
     public Integer getId() {
